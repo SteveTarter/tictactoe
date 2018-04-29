7377db28f8c87241ad197ba0612ef7ad0e22e89c
SteveTarter <38617662+SteveTarter@users.noreply.github.com>
2018-04-29 12:32:20 -0500

    Initial commit
    Initial commit of the Spring Boot Tic Tac Toe application.
    First github project.

---
+++ src/main/java/com/tarterware/tictactoe/Constants.java
@@ -0,0 +1,9 @@
+package com.tarterware.tictactoe;
+
+public class Constants {
+	public static final String GAME_STATE = "gameState";
+	
+	public static final String VIEW_START = "start";
+	public static final String VIEW_PLAYER_SELECT = "playerselect";
+	public static final String VIEW_GAME = "tictactoe";
+}
